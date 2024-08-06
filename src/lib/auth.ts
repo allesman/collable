0a247a17b21dde18e71217d5db2700b0f322d29d
allesman <allesman.stachelx@gmail.com>
2024-08-06 17:47:19 +0200

    move imports to top of auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,18 @@
-import { CLIENT_ID,CLIENT_SECRET } from "$env/static/private";
+import { CLIENT_ID, CLIENT_SECRET } from "$env/static/private";
+import {
+  ClientCredentials,
+  ResourceOwnerPassword,
+  AuthorizationCode,
+} from "simple-oauth2";
 const config = {
   client: {
     id: CLIENT_ID,
-    secret: CLIENT_SECRET
+    secret: CLIENT_SECRET,
   },
   auth: {
-    tokenHost: 'https://api.genius.com'
-  }
+    tokenHost: "https://api.genius.com",
+  },
 };
-
-import { ClientCredentials, ResourceOwnerPassword, AuthorizationCode } from 'simple-oauth2';
 
 const client = new ClientCredentials(config);
 
@@ -18,8 +21,8 @@ export async function getAccessToken() {
     const tokenParams = {};
     const accessToken = await client.getToken(tokenParams);
     return accessToken.token.access_token;
-  } catch (error) {
-    console.error('Access Token error', error.message);
+  } catch (error: any) {
+    console.error("Access Token error", error.message);
     throw error;
   }
 }
