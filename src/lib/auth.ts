56d497aaa0cb873f0b25235470307e6de9b77ea5
allesman <67746449+allesman@users.noreply.github.com>
2024-08-09 00:06:12 +0200

    start with combining auth.ts and geniusApi.ts in one class

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,30 +1,3 @@
-import { CLIENT_ID, CLIENT_SECRET } from "$env/static/private";
-import {
-  ClientCredentials,
-  ResourceOwnerPassword,
-  AuthorizationCode,
-} from "simple-oauth2";
-
 // TODO: put into geniusApi.ts in class
-const config = {
-  client: {
-    id: CLIENT_ID,
-    secret: CLIENT_SECRET,
-  },
-  auth: {
-    tokenHost: "https://api.genius.com",
-  },
-};
 
-const client = new ClientCredentials(config);
 
-export async function getAccessToken() {
-  try {
-    const tokenParams = {};
-    const accessToken = await client.getToken(tokenParams);
-    return accessToken.token.access_token;
-  } catch (error: any) {
-    console.error("Access Token error", error.message);
-    throw error;
-  }
-}
--- src/lib/geniusApi.ts
+++ src/lib/geniusApi.ts
@@ -1,41 +1,76 @@
 import { getAccessToken } from "./auth";
+import { CLIENT_ID, CLIENT_SECRET } from "$env/static/private";
+import {
+  ClientCredentials,
+  ResourceOwnerPassword,
+  AuthorizationCode,
+} from "simple-oauth2";
 // TODO: class pattern
 // TODO: singleton pattern?
-export async function searchGenius(query: string) {
-  const accessToken = await getAccessToken();
-  const response = await fetch(
-    `https://api.genius.com/search?q=${encodeURIComponent(query)}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    }
-  );
-
-  if (!response.ok) {
-    throw new Error(`Error fetching data: ${response.statusText}`);
+class GeniusApi {
+  // auth
+  config = {
+    client: {
+      id: CLIENT_ID,
+      secret: CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: "https://api.genius.com",
+    },
+  };
+  accessToken: string;
+  async constructor() {
+    const client = new ClientCredentials(this.config);
+    this.accessToken = await this.getAccessToken();
   }
 
-  const data = await response.json();
-  return data;
-}
-
-export async function getArtistInfo(query: string) {
-  const accessToken = await getAccessToken();
-  const response = await fetch(
-    `https://api.genius.com/artists/${encodeURIComponent(query)}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+  async getAccessToken() {
+    try {
+      const tokenParams = {};
+      const accessToken = await this.client.getToken(tokenParams);
+      return accessToken.token.access_token;
+    } catch (error: any) {
+      console.error("Access Token error", error.message);
+      throw error;
     }
-  );
-
-  if (!response.ok) {
-    throw new Error(`Error fetching data: ${response.statusText}`);
   }
 
-  const data = await response.json();
-  // console.log(data.response.artist.name);
-  return data;
+  async searchGenius(query: string) {
+    const accessToken = await getAccessToken();
+    const response = await fetch(
+      `https://api.genius.com/search?q=${encodeURIComponent(query)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  }
+
+  async getArtistInfo(query: string) {
+    const accessToken = await getAccessToken();
+    const response = await fetch(
+      `https://api.genius.com/artists/${encodeURIComponent(query)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    // console.log(data.response.artist.name);
+    return data;
+  }
 }
