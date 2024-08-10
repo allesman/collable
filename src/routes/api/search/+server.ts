79a16152a6b90c2bf7a1001d5cedadf4bd650fc0
allesman <67746449+allesman@users.noreply.github.com>
2024-08-10 22:42:50 +0200

    fix artist name being added to query incorrectly

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,17 +1,23 @@
-import { searchGenius, getArtistInfo } from "$lib/geniusApi";
+// import { searchGenius, getArtistInfo } from "$lib/geniusApi";
+import GeniusApi from "$lib/GeniusApi.js";
 import { error } from "@sveltejs/kit";
 
 // TODO: caching?
 export async function GET({ url }) {
+  const geniusApi = await GeniusApi.initialize();
+  console.assert(geniusApi, "GeniusApi not initialized");
   const query = url.searchParams.get("query");
   const artistId = url.searchParams.get("artistId");
   if (!artistId || !query) {
     return error(400, "Query and ArtistId required");
   }
   try {
-    const artist = await getArtistInfo(artistId);
-    console.log(artist.response.artist.name);
-    const data = await searchGenius(query + " " + artist);
+    // TODO: avoid this extra call to get artist name by passing it in from the client
+    const artist = await geniusApi.getArtistInfo(artistId);
+    const finalQuery = query + " " + artist.response.artist.name;
+    console.log("Searching: " + finalQuery);
+    const data = await geniusApi.searchGenius(finalQuery);
+    // const data = await geniusApi.searchGenius(query);
     return new Response(JSON.stringify(data.response.hits), {
       headers: { "Content-Type": "application/json" },
     });
