a02a50c49d3018e03d6fb53d8ca85cc538d69ac8
allesman <67746449+allesman@users.noreply.github.com>
2024-08-14 23:24:07 +0200

    add icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <!-- TODO:add eslint -->
 <script lang="ts">
+  import Icon from "@iconify/svelte";
   import type {
     // SearchResult,
     Song,
@@ -60,90 +61,95 @@
   }
 </script>
 
-<!-- TODO: extract more into components? -->
+<div class="p-10">
+  <!-- TODO: extract more into components? -->
 
-<!-- Name of current artist -->
-<!-- FIXME: add image -->
-<div class="text-center m-2 text-4xl text-primary">{artistObj.name}</div>
+  <!-- Name of current artist -->
+  <!-- FIXME: add image -->
+  <div class="text-center m-2 text-4xl text-primary">{artistObj.name}</div>
 
-<!-- Search Bar -->
-<form
-  method="POST"
-  on:submit|preventDefault={handleSearch}
-  action="?/search"
-  autocomplete="off"
-  class="flex items-center justify-center gap-2 mt-3"
->
-  <input
-    name="songQuery"
-    type="text"
-    placeholder="Enter Song :3"
-    disabled={isLoading || gameStage != 0}
-    class="input input-bordered select-none"
-  />
-  <input type="hidden" name="artistId" bind:value={artistObj.id} />
-  <input type="hidden" name="artistName" bind:value={artistObj.name} />
-  <button
-    type="submit"
-    disabled={isLoading || gameStage != 0}
-    class="btn btn-primary">Search</button
+  <!-- Search Bar -->
+  <form
+    method="POST"
+    on:submit|preventDefault={handleSearch}
+    action="?/search"
+    autocomplete="off"
+    class="flex items-center justify-center gap-2 mt-3"
   >
-</form>
+    <input
+      name="songQuery"
+      type="text"
+      placeholder="Enter Song :3"
+      disabled={isLoading || gameStage != 0}
+      class="input input-bordered select-none"
+    />
+    <input type="hidden" name="artistId" bind:value={artistObj.id} />
+    <input type="hidden" name="artistName" bind:value={artistObj.name} />
+    <button
+      type="submit"
+      disabled={isLoading || gameStage != 0}
+      class="btn btn-primary"
+      class_x="btn btn-primary shadow-md shadow-primary/50"
+      ><Icon icon="mdi:search" class="text-lg" />
+      Search
+    </button>
+  </form>
 
-<!-- Search Results (gameStage 0 only) -->
-{#if gameStage === 0}
-  <div class="flex items-center justify-center mt-10">
-    {#if isLoading}
-      <p class="mt-1">Loading...</p>
-    {:else if searchResults.length > 0}
-      <ul class="flex flex-col items-center justify-center">
-        {#each searchResults as hit, i}
+  <!-- Search Results (gameStage 0 only) -->
+  {#if gameStage === 0}
+    <div class="flex items-center justify-center mt-10">
+      {#if isLoading}
+        <p class="mt-1">Loading...</p>
+      {:else if searchResults.length > 0}
+        <ul class="flex flex-col items-center justify-center">
+          {#each searchResults as hit, i}
+            <li class="w-full text-center m-1">
+              <button
+                on:click={() => handleClickSong(i)}
+                data-index={i}
+                class="btn btn-secondary btn-outline"
+              >
+                <!-- FIXME: add image -->
+                {hit.title}
+                <span class="badge badge-secondary">{hit.artist_names}</span>
+              </button>
+            </li>
+          {/each}
+        </ul>
+      {:else}
+        <!-- TODO: readd and also link to page which enables submission of missing songs -->
+        <!-- <p class="text-gray-400">No Results</p>  -->
+      {/if}
+    </div>
+
+    <!-- Artists of Song (gameStage 1 only) -->
+  {:else if gameStage === 1}
+    <!-- FIXME: close out of this view? and then hide current artist -->
+    <div class="flex flex-col items-center justify-center mt-10">
+      <button class="btn btn-secondary no-animation">
+        {#if song}
+          {song.title}
+        {/if}
+      </button>
+      <ul class="flex flex-col items-center justify-center mt-5">
+        {#each song.combined_artists as artist, i}
           <li class="w-full text-center m-1">
             <button
-              on:click={() => handleClickSong(i)}
+              on:click={() => handleClickArtist(i)}
               data-index={i}
-              class="btn btn-secondary btn-outline"
+              class="btn btn-secondary btn-outline">{artist.name}</button
             >
-              <!-- FIXME: add image -->
-              {hit.title}
-              <span class="badge badge-secondary">{hit.artist_names}</span>
-            </button>
           </li>
         {/each}
       </ul>
-    {:else}
-      <!-- TODO: readd and also link to page which enables submission of missing songs -->
-      <!-- <p class="text-gray-400">No Results</p>  -->
-    {/if}
-  </div>
+    </div>
+    <!-- Error if gameStage has ValueError -->
+  {:else}
+    <p class="text-error text-center">Something went wrong</p>
+  {/if}
 
-  <!-- Artists of Song (gameStage 1 only) -->
-{:else if gameStage === 1}
-  <!-- FIXME: close out of this view? and then hide current artist -->
-  <div class="flex flex-col items-center justify-center mt-10">
-    <button class="btn btn-secondary no-animation">
-      {#if song}
-        {song.title}
-      {/if}
-    </button>
-    <ul class="flex flex-col items-center justify-center mt-5">
-      {#each song.combined_artists as artist, i}
-        <li class="w-full text-center m-1">
-          <button
-            on:click={() => handleClickArtist(i)}
-            data-index={i}
-            class="btn btn-secondary btn-outline">{artist.name}</button
-          >
-        </li>
-      {/each}
-    </ul>
-  </div>
-  <!-- Error if gameStage has ValueError -->
-{:else}
-  <p class="text-error text-center">Something went wrong</p>
-{/if}
-
-<!-- Error Message (if there is one) -->
-{#if error}
-  <p class="text-error text-center">{error}</p>
-{/if}
+  <!-- Error Message (if there is one) -->
+  {#if error}
+    <p class="text-error text-center">{error}</p>
+  {/if}
+</div>
