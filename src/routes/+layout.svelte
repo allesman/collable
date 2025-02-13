d1122561bd312c7e2fafd63dfee3fa7ec67fe2fc
allesman <allesman.stachelx@gmail.com>
2025-02-13 12:24:05 -0800

    vercel speed insights fr this time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import Navbar from "$lib/components/Navbar.svelte";
   import "../app.css";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-
-  injectSpeedInsights();
 </script>
 
 <div class="bg-base-300 min-h-screen">
