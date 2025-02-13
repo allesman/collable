6dfce4aca5595e9b31e8762be111a0d5c74303c5
allesman <allesman.stachelx@gmail.com>
2025-02-13 08:53:37 -0800

    refactor game creation logic

--- src/lib/createNewGames.cjs
+++ src/lib/createNewGames.cjs
@@ -2,78 +2,63 @@ const { pushToDB, fetchData, getAllData, getLatestDate } = require("./dbUtil.cjs
 const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
+
+let artistsList = [];
 // import type { DailyGame } from "./types";
 
-// module.exports = createNewGame;
-
-async function createNewBatch(untilDateStr) {
+async function createNewGamesUntil(untilDateStr) {
     date = new Date(untilDateStr);
     // get latest date with data
     data = await getAllData();
-
-    const artistsFilePath = path.join(__dirname, '../../static/artists.csv');
-    let artistsList = [];
-
-    fs.createReadStream(artistsFilePath)
-        .pipe(csv.parse({ headers: false }))
-        .on('error', error => console.error(error))
-        .on('data', (row) => { artistsList.push(row[0]); })
-        .on('end', () => {
-            console.log(`Parsed ${artistsList.length} rows`);
-            console.assert(artistsList.length > 0, 'Artists list is empty');
-            let latestDateStr = getLatestDate(data);
-            let latestDate = new Date(latestDateStr);
-            while (latestDate < date) {
-
-                const getRandomArtist = () => artistsList[Math.floor(Math.random() * artistsList.length)];
-                const addGame = (dateStr) => {
-                    do {
-                        startArtist = getRandomArtist();
-                        goalArtist = getRandomArtist();
-                    }
-                    while (startArtist === goalArtist);
-                    const dailyGameEntry = {
-                        startArtist: startArtist,
-                        goalArtist: goalArtist,
-                    };
-                    pushToDB(dailyGameEntry, dateStr);
-                    console.log(`Created ${startArtist} -> ${goalArtist}`);
-                }
-                addGame(latestDateStr);
-                latestDate.setDate(latestDate.getDate() + 1);
-                latestDateStr = latestDate.toISOString().substring(0, 10);
-                // process.exit();
-            }
-        });
+    let latestDateStr = getLatestDate(data);
+    let latestDate = new Date(latestDateStr);
+    while (latestDate < date) {
+        latestDate.setDate(latestDate.getDate() + 1);
+        latestDateStr = latestDate.toISOString().substring(0, 10);
+        createNewGame(latestDateStr);
+    }
 }
 
-function createNewGame(dateStr = null) {
-    const artistsFilePath = path.join(__dirname, '../../static/artists.csv');
-    let artistsList = [];
+async function createNewGame(dateStr = null) {
+    let artistsList = await getArtistList();
+    const getRandomArtist = () => artistsList[Math.floor(Math.random() * artistsList.length)];
+    do {
+        startArtist = getRandomArtist();
+        goalArtist = getRandomArtist();
+    }
+    while (startArtist === goalArtist);
+    const dailyGameEntry = {
+        startArtist: startArtist,
+        goalArtist: goalArtist,
+    };
+    pushToDB(dailyGameEntry, dateStr);
+    console.log(`Created ${startArtist} -> ${goalArtist}`);
+}
 
-    fs.createReadStream(artistsFilePath)
-        .pipe(csv.parse({ headers: false }))
-        .on('error', error => console.error(error))
-        .on('data', (row) => { artistsList.push(row[0]); })
-        .on('end', () => {
-            console.log(`Parsed ${artistsList.length} rows`);
-            console.assert(artistsList.length > 0, 'Artists list is empty');
-            const getRandomArtist = () => artistsList[Math.floor(Math.random() * artistsList.length)];
-            do {
-                startArtist = getRandomArtist();
-                goalArtist = getRandomArtist();
-            }
-            while (startArtist === goalArtist);
-            const dailyGameEntry = {
-                startArtist: startArtist,
-                goalArtist: goalArtist,
-            };
-            pushToDB(dailyGameEntry, dateStr);
-            console.log(`Created ${startArtist} -> ${goalArtist}`);
-            // process.exit();
-        });
+async function getArtistList() {
+    if (artistsList.length > 0) {
+        return artistsList;
+    }
+    const artistsFilePath = path.join(__dirname, '../../static/artists.csv');
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(artistsFilePath)
+            .pipe(csv.parse({ headers: false }))
+            .on('error', error => {
+                console.error(error);
+                reject(error);
+            })
+            .on('data', (row) => { artistsList.push(row[0]); })
+            .on('end', () => {
+                console.log(`Parsed ${artistsList.length} rows`);
+                console.assert(artistsList.length > 0, 'Artists list is empty');
+                resolve(artistsList);
+            });
+    });
 }
 
 if (require.main === module) {
-    createNewBatch("2025-03-01");
+    createNewGamesUntil("2025-04-01");
 }
+
+// module.exports = createNewGame;
