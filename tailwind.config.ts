a7857d49ae0f4ef1e9aa1d96142471b34c3a814c
allesman <allesman.stachelx@gmail.com>
2025-02-16 11:19:26 -0800

    drop shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,34 @@
 import type { Config } from "tailwindcss";
 import themes from "daisyui/src/theming/themes";
-
+const plugin = require('tailwindcss/plugin')
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+        soft: '0 0px 2px var(--tw-shadow-color)',
+        softer: '0 0px 6px var(--tw-shadow-color)',
+      },
+    },
   },
 
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 
   daisyui: {
     themes:
