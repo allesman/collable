b2bcd1422ca857a635457262e6069ec427653441
allesman <allesman.stachelx@gmail.com>
2025-02-18 22:58:53 -0800

    add tutorial modal and button to Navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,40 +2,29 @@
   import ThemeSwitch from "./ThemeSwitch.svelte";
   import Icon from "@iconify/svelte";
   import CreateCustomGame from "./CreateCustomGame.svelte";
+  import Tutorial from "./Tutorial.svelte";
 
   let createCustomGame: CreateCustomGame;
+  let tutorial: Tutorial;
 </script>
 
 <CreateCustomGame bind:this={createCustomGame} />
+<Tutorial bind:this={tutorial} />
 <div class="navbar sticky top-0 z-50 bg-base-200">
-  <div class="navbar-start pl-2">
+  <div class="navbar-start pl-2 gap-2">
     <button
-      class="btn btn-m btn-outline rounded-xl"
+      class="btn btn-m btn-neutral rounded-xl"
       on:click={() => createCustomGame.openModal()}
     >
       <Icon icon="mdi:sparkles-outline" class="text-xl" />Custom
     </button>
     <!-- TODO: add hard mode toggle (hard mode:no default songs?? or songs with artist credits only) -->
-    <!-- TODO: add button to show tutorial popup (should also show up on first visit) -->
-    <!-- Identify the artist matchup:
-
-Kendrick Lamar
-
-Chappell Roan
-
-2
-
-Tap on the related artists that you think will get you closer to the target artist. Press and hold on an artist to hear a sample of their music. The less steps it takes you, the better your score!
-
-If you get stuck, you can Reset, use the Hint, or Give Up.
-
-3
-
-Select the target artist once they pop up or you'll miss it! Have fun!
-Clicking an artist box
-What's a related artist?
-
-Related artists are based on the "Fans Also Like" sections on music services. They use data from listeners of an artist to identify other artists that fans frequently listen to.  -->
+    <button
+      class="btn btn-m btn-neutral rounded-xl"
+      on:click={() => tutorial.openModal()}
+    >
+      <Icon icon="mdi:help-circle-outline" class="text-xl" />How To Play
+    </button>
   </div>
 
   <a
