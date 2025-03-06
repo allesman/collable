ab8c861b846314702ea317c91699ac6f120c0819
allesman <allesman.stachelx@gmail.com>
2025-03-06 18:16:23 -0500

    feat: add featuringOnly parameter to getDefaultSongs for hint

--- src/lib/gameUtils.ts
+++ src/lib/gameUtils.ts
@@ -2,7 +2,7 @@ import { error } from "@sveltejs/kit";
 import GeniusApi from "./GeniusApi";
 import type { Song } from "./types";
 
-export async function getDefaultSongs(artistId: string, amount: number = 10): Promise<Song[]> {
+export async function getDefaultSongs(artistId: string, amount: number = 10, featuringOnly: boolean = false): Promise<Song[]> {
     // returns default songs for artist with added combined artists for each song
     const geniusApi = await GeniusApi.initialize();
     console.assert(geniusApi, "GeniusApi not initialized");
@@ -10,9 +10,30 @@ export async function getDefaultSongs(artistId: string, amount: number = 10): Pr
         throw error(422, { message: "Artist ID not provided" });
     }
     try {
-        // we only get and return the <=10 new songs aka those from the last page
         const songs = await geniusApi.getSongs(artistId, 10, Math.ceil(amount / 10));
-        return songs;
+        let returnSongs = [];
+        if (featuringOnly) {
+            // for the hint popup, it would be bad to show songs that the artist is not featured in (player can't use them)
+            // FIXME: also get 10 songs
+            for (let i = 0; i < songs.length; i++) {
+                const primary = songs[i].primary_artists;
+                const features = songs[i].featured_artists;
+                const combinedArtists = primary.concat(features);
+                for (const element of combinedArtists) {
+                    if (element.id == artistId) {
+                        returnSongs.push(songs[i]);
+                        break;
+                    }
+                }
+            }
+            // TODO: filter out results that aren't songs??
+        }
+        else {
+            // for the normal songs displayed when playing
+            // we only get and return the <=10 new songs aka those from the last page
+            returnSongs = songs;
+        }
+        return returnSongs;
     } catch (e) {
         console.error(e);
         return error(500, "Error while fetching defaultSongs");
