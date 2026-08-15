376e0bb69c4dadbf8cb14d85b042ebebc5c362d5
merlin <allesman.stachelx@gmail.com>
2026-08-15 21:57:32 +0200

    remove highlighting of donation button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -77,18 +77,13 @@
       <!-- New Daisyui Stuff, not working in 4.x -->
       <!-- <span class="indicator-item badge badge-primary text-xs">New</span> -->
       <!-- Old implementation -->
-      <span
-        class="indicator-item badge badge-warning badge-xs {isFirstVisit
-          ? 'hidden'
-          : ''}"
-      ></span>
       <a
         href="https://www.ko-fi.com/collable"
         target="_blank"
         aria-label="Donation Link"
         class="btn btn-m btn-neutral rounded-xl"
       >
-        <Icon icon="mdi:gift-outline" class="text-xl text-warning "></Icon>
+        <Icon icon="mdi:gift-outline" class="text-xl "></Icon>
       </a>
     </div>
     <!-- <a href="https://www.tiktok.com/collable_app/" target="_blank">
